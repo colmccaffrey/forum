d49edbb41802ab11a991ad6a3ace386c304796cf
Col McCaffrey <colleen31@gmail.com>
2015-08-14 22:32:32 -0400

    added username to recent comments

--- app.js
+++ app.js
@@ -24,10 +24,7 @@ app.get('/forum', function(req, res){ //renders index  with navigation options
 });
 
 app.get('/forum/topics', function(req, res){ //renders page with a list of all topics by latest first- option to input new topic
-	//db.all("SELECT * FROM topics ORDER BY id DESC", function(err, topics){
-		//db.all("SELECT * FROM users", function(err, authors){
-		//db.all("SELECT users.name AS user_name FROM users INNER JOIN topics WHERE users.id= topics.user_id", function(err, author){
-		db.all("SELECT topics.title AS title, topics.user_id AS user_id, topics.id AS id, topics.votes AS votes, users.name AS user_name FROM topics INNER JOIN users ON topics.user_id = users.id ORDER BY topics.id DESC", function(err, topics){
+	db.all("SELECT topics.title AS title, topics.user_id AS user_id, topics.id AS id, topics.votes AS votes, users.name AS user_name FROM topics INNER JOIN users ON topics.user_id = users.id ORDER BY topics.id DESC", function(err, topics){
 		if(err){
 			throw err;
 		};
@@ -46,7 +43,7 @@ app.get('/forum/users', function(req, res){ //test function to track users
 });
 
 app.get('/forum/topics/popular', function(req, res){ //renders page with a list of all topics by vote count, popular first- option to input new topic
-	db.all("SELECT * FROM topics ORDER BY votes DESC", function(err, topics){
+	db.all("SELECT topics.title AS title, topics.user_id AS user_id, topics.id AS id, topics.votes AS votes, users.name AS user_name FROM topics INNER JOIN users ON topics.user_id = users.id ORDER BY topics.votes DESC", function(err, topics){
 		if(err){
 			throw err;
 		};
@@ -56,7 +53,7 @@ app.get('/forum/topics/popular', function(req, res){ //renders page with a list 
 });
 
 app.get('/forum/comments/recent', function(req, res){ //renders page with a list of recent  comments and links topic page (list of comments by topic) the comment is posted in are posted in - optiont o add new topic
-	db.all("SELECT comments.content AS content, topics.title AS topic FROM comments INNER JOIN topics ON comments.topic_id = topics.id ORDER BY comments.id DESC", function(err, recent){ 
+	db.all("SELECT comments.content AS content, comments.user_id AS user_id, users.name AS user_name, users.id AS id, topics.title AS topic FROM comments INNER JOIN topics INNER JOIN users ON comments.topic_id = topics.id and comments.user_id = users.id ORDER BY comments.id DESC", function(err, recent){ 
 		if(err){
 			throw err;
 		};
